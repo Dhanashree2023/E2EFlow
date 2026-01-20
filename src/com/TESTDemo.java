35a75602fda4da57a2a7ff7700478cb89d515e2e
Dhanashree2023 <Prachi123451@gmail.com>
2026-01-20 20:28:40 +0530

    First

---
+++ src/com/TESTDemo.java
@@ -0,0 +1,15 @@
+package com;
+
+public class TESTDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("TC 1");
+		System.out.println("TC 2");
+		System.out.println("TC 3");
+		System.out.println("TC 4");
+		System.out.println("TC 5");
+	}
+
+}
